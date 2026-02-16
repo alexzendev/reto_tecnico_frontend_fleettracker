534d4d3cefb5196ea373cb203eb25b95785f6df6
54Alexzen <alexzen.develop@gmail.com>
2026-02-16 16:42:59 -0600

    feat: envolver rutas en el componente Layout para mejorar la estructura de la aplicación

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./app/pages/home";
+import { Layout } from "./app/layout/layout";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/vehicles/:id" element={<div>Vehicle Details</div>} />
-        <Route path="/vehicles/new" element={<div>New Vehicle</div>} />
-        <Route path="*" element={<div>404 Not Found</div>} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/vehicles/:id" element={<div>Vehicle Details</div>} />
+          <Route path="/vehicles/new" element={<div>New Vehicle</div>} />
+          <Route path="*" element={<div>404 Not Found</div>} />
+        </Routes>
+      </Layout>
     </BrowserRouter>
   );
 }
