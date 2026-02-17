e71e647161c4961ab0e70a40723c0ad998e31f80
54Alexzen <alexzen.develop@gmail.com>
2026-02-17 15:24:21 -0600

    feat: integrar QueryClient y Toaster en el componente App

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,27 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./app/pages/home";
 import { Layout } from "./app/layout/layout";
 import NotFound from "./app/pages/not-found";
+import NewVehicle from "./app/pages/new-vehicle";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "sonner";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/vehicles/:id" element={<div>Vehicle Details</div>} />
-          <Route path="/vehicles/new" element={<div>New Vehicle</div>} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Layout>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/vehicles/new" element={<NewVehicle />} />
+            <Route path="/vehicles/:id" element={<div>Vehicle Details</div>} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+      <Toaster richColors position="top-center" />
+    </QueryClientProvider>
   );
 }
 
