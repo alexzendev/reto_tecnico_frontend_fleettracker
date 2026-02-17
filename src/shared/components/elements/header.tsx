3eb4ace658e9ae774c0db71da5adbaa8774ccea3
54Alexzen <alexzen.develop@gmail.com>
2026-02-16 18:28:45 -0600

    feat: ajustar estilos del componente Header para mejorar la presentación

--- src/shared/components/elements/header.tsx
+++ src/shared/components/elements/header.tsx
@@ -2,9 +2,9 @@ import { Imagotipo } from "../icons/imagotipo";
 
 export const Header = () => {
   return (
-    <header className="py-4 border-b border-stone-200">
-      <div className="container mx-auto flex justify-center">
-        <Imagotipo className="w-48" />
+    <header className="h-16 border-b border-stone-200">
+      <div className="container mx-auto flex justify-center items-center h-full">
+        <Imagotipo className="w-40" />
       </div>
     </header>
   );
