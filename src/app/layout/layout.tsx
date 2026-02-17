666a2f64a2b84076c435afca2c54490c21f9f257
54Alexzen <alexzen.develop@gmail.com>
2026-02-16 18:28:57 -0600

    feat: mejorar estructura y estilos del componente Layout para una mejor presentación

--- src/app/layout/layout.tsx
+++ src/app/layout/layout.tsx
@@ -2,9 +2,13 @@ import { Header } from "@/shared/components/elements/header";
 
 export const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="bg-stone-50">
+    <div className="bg-stone-50 text-stone-800">
       <Header />
-      <main className="container mx-auto">{children}</main>
+      <main className="mx-2">
+        <div className="container mx-auto border-x border-stone-200">
+          {children}
+        </div>
+      </main>
     </div>
   );
 };
