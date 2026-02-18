e1f45b7b4edc517f674ddadd4e3322ad3868c83b
54Alexzen <alexzen.develop@gmail.com>
2026-02-18 00:06:42 -0600

    feat: agregar botón de cerrar sesión en el sidebar y mejorar diseño

--- src/shared/components/elements/sidebar.tsx
+++ src/shared/components/elements/sidebar.tsx
@@ -1,6 +1,7 @@
 import { uiStore } from "@/shared/stores/ui-store";
 import { ThemeToggle } from "./theme-toggle";
-import { ChevronLeft } from "lucide-react";
+import { ChevronLeft, LogOut } from "lucide-react";
+import { Divider } from "../ui/divider";
 
 export const Sidebar = () => {
   const { isSidebarOpen, toggleSidebar } = uiStore();
@@ -16,21 +17,34 @@ export const Sidebar = () => {
           />
         </button>
       </div>
-      <div className="w-64 border-t border-stone-200 dark:border-stone-700 divide divide-stone-200 dark:divide-stone-700 p-4">
-        <div className="flex flex-row items-center md:gap-2 gap-1">
-          <div className="bg-stone-200 rounded-full lg:size-10 size-8 flex items-center justify-center">
+      <div className="w-64 border-t border-stone-200 dark:border-stone-700 divide divide-stone-200 dark:divide-stone-700 lg:px-4 px-2">
+        <div className="flex flex-row items-center md:gap-2 gap-1 lg:py-4 py-2">
+          <div className="bg-primary text-stone-200 rounded-full lg:size-9 size-8 flex items-center justify-center">
             <span className="font-bold lg:text-base sm:text-sm text-xs">A</span>
           </div>
           <div>
-            <h2 className="lg:text-sm text-xs font-semibold">Alexis Jimenez</h2>
-            <p className="lg:text-xs text-1.5xs text-stone-500 dark:text-stone-400">
-              Administrador
+            <h2 className="lg:text-xs text-1.5xs font-semibold">
+              Alexis Jimenez
+            </h2>
+            <p className="lg:text-1.5xs text-2xs text-stone-500 dark:text-stone-400">
+              Usuario
             </p>
           </div>
         </div>
 
-        <div></div>
         <ThemeToggle />
+        <div>
+          <Divider />
+          <div className="lg:py-4 py-2">
+            <button
+              className="flex items-center lg:gap-2 gap-1 hover:bg-stone-200 dark:hover:bg-stone-800 rounded-md transition-colors duration-200 cursor-pointer w-full p-2 disabled:cursor-not-allowed disabled:hover:bg-transparent"
+              disabled={true}
+            >
+              <LogOut className="size-3.5 shrink-0" />
+              <span className="lg:text-xs text-1.5xs">Cerrar sesión</span>
+            </button>
+          </div>
+        </div>
       </div>
     </aside>
   );
