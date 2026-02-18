1f28b233192f973d2cf973e40f018a4f74333713
54Alexzen <alexzen.develop@gmail.com>
2026-02-17 23:24:53 -0600

    feat: agregar soporte para modo oscuro en el componente Header

--- src/shared/components/elements/header.tsx
+++ src/shared/components/elements/header.tsx
@@ -2,7 +2,7 @@ import { Imagotipo } from "../icons/imagotipo";
 
 export const Header = () => {
   return (
-    <header className="h-16 border-b border-stone-200">
+    <header className="h-16 border-b border-stone-200 dark:border-stone-700">
       <div className="container mx-auto flex justify-center items-center h-full">
         <Imagotipo className="w-40" />
       </div>
