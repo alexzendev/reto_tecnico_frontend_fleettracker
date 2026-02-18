119bee2a895f2e735480122dd1f01f086f3489bb
54Alexzen <alexzen.develop@gmail.com>
2026-02-17 23:25:26 -0600

    feat: agregar formulario de creación de vehículos en la página NewVehicle

--- src/app/pages/new-vehicle.tsx
+++ src/app/pages/new-vehicle.tsx
@@ -1,7 +1,10 @@
+import { CreateVehicleForm } from "@/modules/vehicles/components/create-vehicle-form";
+
 export default function NewVehicle() {
-    return (
-        <section>
-            <h1>New Vehicle</h1>
-        </section>
-    )
+  return (
+    <section>
+      <h1>New Vehicle</h1>
+      <CreateVehicleForm />
+    </section>
+  );
 }
