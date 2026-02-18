6648f805cece1cc5d5402c4db0339cf1edfd569a
54Alexzen <alexzen.develop@gmail.com>
2026-02-17 23:25:37 -0600

    feat: mejorar diseño del layout agregando soporte para modo oscuro y sidebar

--- src/app/layout/layout.tsx
+++ src/app/layout/layout.tsx
@@ -1,12 +1,14 @@
 import { Header } from "@/shared/components/elements/header";
+import { Sidebar } from "@/shared/components/elements/sidebar";
 
 export const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="bg-stone-50 text-stone-800">
+    <div className="bg-stone-50 dark:bg-stone-900 text-stone-800 dark:text-stone-200">
       <Header />
-      <main className="mx-2">
-        <div className="container mx-auto border-x border-stone-200">
-          {children}
+      <main className="mx-2 flex">
+        <div className="container mx-auto border-x border-stone-200 dark:border-stone-700 flex min-h-[calc(100vh-4rem)]">
+          <Sidebar />
+          <section>{children}</section>
         </div>
       </main>
     </div>
