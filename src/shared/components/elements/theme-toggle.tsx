7d8609a5ab3ef951de984c61239ea9833ceb84f3
54Alexzen <alexzen.develop@gmail.com>
2026-02-18 16:51:30 -0600

    feat: refactor ThemeToggle para usar configuración de botones desde DATA_UI y mejorar la accesibilidad

--- src/shared/components/elements/theme-toggle.tsx
+++ src/shared/components/elements/theme-toggle.tsx
@@ -1,36 +1,34 @@
 import { useTheme } from "@/shared/hooks/useTheme";
-import { Monitor, Moon, Sun } from "lucide-react";
 import { Divider } from "../ui/divider";
+import { DATA_UI } from "@/shared/utils/data-ui";
+import { uiStore } from "@/shared/stores/ui-store";
 
 export const ThemeToggle = () => {
+  const { isSidebarOpen } = uiStore();
   const { theme, setTheme } = useTheme();
-
-  const buttons = [
-    { label: "Sistema", value: "system", icons: Monitor },
-    { label: "Claro", value: "light", icons: Sun },
-    { label: "Oscuro", value: "dark", icons: Moon },
-  ] as const;
-
+  const { theme: buttons } = DATA_UI;
   return (
     <>
-      <Divider text="Aspecto" />
+      <Divider text={isSidebarOpen ? "Aspecto" : ""} />
       <div className="flex flex-col py-4">
         {buttons.map((btn) => (
           <button
             key={btn.label}
-            onClick={() => setTheme(btn.value)}
-            className="flex items-center lg:gap-2 gap-1 hover:bg-stone-200 dark:hover:bg-stone-800 rounded-md transition-colors duration-200 cursor-pointer w-full p-2 disabled:cursor-not-allowed disabled:hover:bg-transparent"
+            onClick={() => setTheme(btn.value as "light" | "dark" | "system")}
+            className="flex items-center lg:gap-2 gap-1 hover:bg-stone-200 dark:hover:bg-stone-800 rounded-md transition-colors duration-200 cursor-pointer w-full lg:p-2 p-1.5 disabled:cursor-not-allowed disabled:hover:bg-transparent"
             aria-label={`Cambiar a tema ${btn.label}`}
             disabled={theme === btn.value}
           >
             <btn.icons
-              className={`size-3.5 shrink-0 ${theme === btn.value ? "text-primary" : ""}`}
+              className={`size-4 shrink-0 ${theme === btn.value ? "text-primary" : ""}`}
             />
-            <span
-              className={`lg:text-xs text-1.5xs ${theme === btn.value ? "text-primary font-medium" : ""}`}
-            >
-              {btn.label}
-            </span>
+            {isSidebarOpen && (
+              <span
+                className={`lg:text-xs text-1.5xs ${theme === btn.value ? "text-primary font-medium" : ""}`}
+              >
+                {btn.label}
+              </span>
+            )}
           </button>
         ))}
       </div>
