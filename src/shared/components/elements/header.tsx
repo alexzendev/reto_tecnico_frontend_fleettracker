62acc16c6d9f05948e4dce4fc4d5f3483bc63e9f
54Alexzen <alexzen.develop@gmail.com>
2026-02-18 21:56:11 -0600

    feat: actualizar el componente Header para mostrar el nombre del usuario y la fecha actual

--- src/shared/components/elements/header.tsx
+++ src/shared/components/elements/header.tsx
@@ -1,11 +1,20 @@
-import { Imagotipo } from "../icons/imagotipo";
+import { uiStore } from "@/shared/stores/ui-store";
 
 export const Header = () => {
+  const { user } = uiStore();
+  const date = new Date().toLocaleDateString("es-ES", {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
   return (
-    <header className="h-16 border-b border-stone-200 dark:border-stone-700">
-      <div className="container mx-auto flex justify-center items-center h-full">
-        <Imagotipo className="w-40" />
-      </div>
+    <header className="sticky top-0 border-b border-stone-200 dark:border-stone-700 w-full sm:p-4 p-2">
+      <div className="h-7 mb-1" />
+      <h1 className="font-semibold lg:text-2xl sm:text-xl text-lg tracking-tighter mt-1">
+        Bienvenido, {user.name}
+      </h1>
+      <p className="lg:text-1.5xs text-2xs text-stone-500 capitalize">{date}</p>
     </header>
   );
 };
