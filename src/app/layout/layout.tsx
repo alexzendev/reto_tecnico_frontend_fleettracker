077417fee7ed88d3bc80bab7fdce562aca2d9a33
54Alexzen <alexzen.develop@gmail.com>
2026-02-18 23:03:05 -0600

    fix: corregir errores tipográficos y ajustar estilos en el componente Layout

--- src/app/layout/layout.tsx
+++ src/app/layout/layout.tsx
@@ -7,14 +7,15 @@ import { Outlet } from "react-router-dom";
 export const Layout = () => {
   const { isSidebarOpen, toggleSidebar } = uiStore();
   return (
-    <div className="bg-stone-50 dark:bg-stone-900 text-stone-800 dark:text-stone-200 min-h-svh">
+    <div className="bg-stone-50 dark:bg-stone-900 text-stone-800 dark:text-stone-200">
       <main>
         <section className="flex flex-row w-full">
           <Sidebar />
-          <div className="w-full realative">
+          <div className="w-full relative">
             <button
               onClick={toggleSidebar}
-              className="fixed top-3 left-3 z-50 cursor-pointer hover:bg-stone-200 dark:hover:bg-stone-800 rounded-md p-1"
+              className="fixed top-4 sm:left-4 left-2 z-50 cursor-pointer hover:bg-stone-200 dark:hover:bg-stone-800 rounded-md p-1"
+              aria-label="Colapse sidebar"
             >
               <PanelLeftClose
                 className={`size-5 ${isSidebarOpen ? "" : "rotate-180"}`}
