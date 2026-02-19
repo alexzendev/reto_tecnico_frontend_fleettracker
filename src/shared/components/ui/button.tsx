5d39046dc8b41c538da01718b57131bd7469e820
54Alexzen <alexzen.develop@gmail.com>
2026-02-19 16:12:40 -0600

    feat: permitir propiedades adicionales en el componente Button

--- src/shared/components/ui/button.tsx
+++ src/shared/components/ui/button.tsx
@@ -1,10 +1,15 @@
-interface ButtonProps {
-  children: React.ReactNode;
+import type { ButtonHTMLAttributes, ReactNode } from "react";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: ReactNode;
 }
 
-export const Button = ({ children }: ButtonProps) => {
+export const Button = ({ children, ...props }: ButtonProps) => {
   return (
-    <button className="bg-primary hover:bg-secondary text-stone-100 px-5 py-2.5 rounded-full sm:text-xs text-1.5xs uppercase font-semibold flex items-center justify-center gap-1 cursor-pointer transition-all duration-200 group">
+    <button
+      {...props}
+      className="bg-primary hover:bg-secondary text-stone-100 px-5 py-2.5 rounded-full sm:text-xs text-1.5xs uppercase font-semibold flex items-center justify-center gap-1 cursor-pointer transition-all duration-200 group"
+    >
       {children}
     </button>
   );
