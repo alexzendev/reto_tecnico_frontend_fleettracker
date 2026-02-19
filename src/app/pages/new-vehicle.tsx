e4351de512a011c664b767158076f0fab0d3f277
54Alexzen <alexzen.develop@gmail.com>
2026-02-19 17:03:53 -0600

    feat: mejorar formulario de nuevo vehículo con validaciones y elementos visuales

--- src/app/pages/new-vehicle.tsx
+++ src/app/pages/new-vehicle.tsx
@@ -8,6 +8,7 @@ import { Header } from "@/shared/components/elements/header";
 import { Button } from "@/shared/components/ui/button";
 import { Input } from "@/shared/components/ui/input";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { CarFront, Loader } from "lucide-react";
 import { useForm } from "react-hook-form";
 
 export default function NewVehicle() {
@@ -28,16 +29,15 @@ export default function NewVehicle() {
     });
   };
 
-  const currentYear = new Date().getFullYear();
   return (
-    <section>
+    <section className="min-h-svh flex flex-col">
       <Header title="Agregar nuevo vehículo" />
-      <div className="lg:p-4 p-2">
+      <div className="flex-1 flex items-center justify-center p-4">
         <form
           onSubmit={handleSubmit(onSubmit)}
-          className="max-w-2xl space-y-3 mx-auto"
+          className="max-w-2xl w-full space-y-3"
         >
-          <div className="flex flex-row gap-4">
+          <div className="flex md:flex-row flex-col lg:gap-4 gap-2">
             <Input
               placeholder="KJX-482-B"
               label="Placa vehícular"
@@ -52,7 +52,7 @@ export default function NewVehicle() {
             />
           </div>
 
-          <div className="flex flex-row gap-4">
+          <div className="flex md:flex-row flex-col lg:gap-4 gap-2">
             <Input
               placeholder="Corolla"
               label="Modelo"
@@ -63,7 +63,6 @@ export default function NewVehicle() {
             <Input
               type="number"
               min={0}
-              max={currentYear}
               placeholder="2020"
               label="Año"
               error={errors.año?.message}
@@ -71,7 +70,7 @@ export default function NewVehicle() {
             />
           </div>
 
-          <div className="flex flex-row gap-4">
+          <div className="flex md:flex-row flex-col lg:gap-4 gap-2">
             <Input
               placeholder="Rojo"
               label="Color"
@@ -89,19 +88,43 @@ export default function NewVehicle() {
             />
           </div>
 
-          <div className="">
-            <select {...register("estado")}>
+          <div className="flex flex-col gap-1 w-full mb-10">
+            <label
+              htmlFor="estado"
+              className="sm:text-xs text-1.5xs uppercase font-semibold"
+            >
+              Estado
+            </label>
+            <select
+              id="estado"
+              {...register("estado")}
+              className="px-3 py-2 sm:text-xs text-1.5xs border rounded-full outline-none autofill-input dark:autofill-input-dark border-stone-200 dark:border-stone-700 bg-stone-100 dark:bg-stone-800 appearance-none w-full"
+            >
               <option value="">Selecciona un estado</option>
               <option value="Disponible">Disponible</option>
               <option value="En ruta">En ruta</option>
               <option value="En mantenimiento">En mantenimiento</option>
               <option value="Fuera de servicio">Fuera de servicio</option>
             </select>
-            {errors.estado && <span>{errors.estado.message}</span>}
+            {errors.estado && (
+              <span className="sm:text-1.5xs text-2xs text-red-400 dark:text-red-600">
+                *{errors.estado.message}
+              </span>
+            )}
           </div>
 
           <Button type="submit" disabled={isPending}>
-            {isPending ? "Creando..." : "Agregar vehículo"}
+            {isPending ? (
+              <div className="flex items-center gap-1">
+                <span>Creando vehículo</span>
+                <Loader className="animate-spin size-4" />
+              </div>
+            ) : (
+              <div className="flex items-center gap-1">
+                <span>Agregar vehículo</span>
+                <CarFront className="size-4" />
+              </div>
+            )}
           </Button>
         </form>
       </div>
