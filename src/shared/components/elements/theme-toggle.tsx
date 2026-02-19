fdb557ee8a828b6b8ae172dc62c2b177182a98d2
54Alexzen <alexzen.develop@gmail.com>
2026-02-18 21:55:53 -0600

    feat: integrar Tooltip en el componente ThemeToggle para mejorar la accesibilidad y presentación

--- src/shared/components/elements/theme-toggle.tsx
+++ src/shared/components/elements/theme-toggle.tsx
@@ -2,6 +2,7 @@ import { useTheme } from "@/shared/hooks/useTheme";
 import { Divider } from "../ui/divider";
 import { DATA_UI } from "@/shared/utils/data-ui";
 import { uiStore } from "@/shared/stores/ui-store";
+import { Tooltip } from "../ui/tooltip";
 
 export const ThemeToggle = () => {
   const { isSidebarOpen } = uiStore();
@@ -9,27 +10,28 @@ export const ThemeToggle = () => {
   const { theme: buttons } = DATA_UI;
   return (
     <>
-      <Divider text={isSidebarOpen ? "Aspecto" : ""} />
-      <div className="flex flex-col py-4">
+      <Divider text="Aspecto" />
+      <div className="flex flex-col">
         {buttons.map((btn) => (
-          <button
+          <Tooltip
             key={btn.label}
-            onClick={() => setTheme(btn.value as "light" | "dark" | "system")}
-            className="flex items-center lg:gap-2 gap-1 hover:bg-stone-200 dark:hover:bg-stone-800 rounded-md transition-colors duration-200 cursor-pointer w-full lg:p-2 p-1.5 disabled:cursor-not-allowed disabled:hover:bg-transparent"
-            aria-label={`Cambiar a tema ${btn.label}`}
-            disabled={theme === btn.value}
+            content={btn.label}
+            position="right"
+            disabled={isSidebarOpen}
+            arrow={false}
           >
-            <btn.icons
-              className={`size-4 shrink-0 ${theme === btn.value ? "text-primary" : ""}`}
-            />
-            {isSidebarOpen && (
-              <span
-                className={`lg:text-xs text-1.5xs ${theme === btn.value ? "text-primary font-medium" : ""}`}
-              >
-                {btn.label}
-              </span>
-            )}
-          </button>
+            <button
+              onClick={() => setTheme(btn.value as "light" | "dark" | "system")}
+              className={`flex items-center lg:gap-2 gap-1 hover:bg-stone-200 dark:hover:bg-stone-800 rounded-md transition-colors duration-200 cursor-pointer w-full lg:p-2 p-1.5 disabled:cursor-not-allowed disabled:hover:bg-transparent ${theme === btn.value ? "text-primary font-medium" : ""}`}
+              aria-label={`Cambiar a tema ${btn.label}`}
+              disabled={theme === btn.value}
+            >
+              <btn.icons className="size-4 shrink-0" />
+              {isSidebarOpen && (
+                <span className="lg:text-xs text-1.5xs ">{btn.label}</span>
+              )}
+            </button>
+          </Tooltip>
         ))}
       </div>
     </>
