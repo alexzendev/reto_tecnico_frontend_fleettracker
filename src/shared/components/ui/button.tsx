08b42861eb6643d4ed7150c848c74a20b5f7f4c9
54Alexzen <alexzen.develop@gmail.com>
2026-02-19 17:04:15 -0600

    feat: agregar soporte para estado deshabilitado en el componente Button

--- src/shared/components/ui/button.tsx
+++ src/shared/components/ui/button.tsx
@@ -8,7 +8,8 @@ export const Button = ({ children, ...props }: ButtonProps) => {
   return (
     <button
       {...props}
-      className="bg-primary hover:bg-secondary text-stone-100 px-5 py-2.5 rounded-full sm:text-xs text-1.5xs uppercase font-semibold flex items-center justify-center gap-1 cursor-pointer transition-all duration-200 group"
+      className="bg-primary hover:bg-secondary text-stone-100 px-5 py-2.5 rounded-full sm:text-xs text-1.5xs uppercase font-semibold flex items-center justify-center gap-1 cursor-pointer transition-all duration-200 disabled:opacity-80 disabled:cursor-not-allowed disabled:hover:bg-primary w-full group"
+      disabled={props.disabled}
     >
       {children}
     </button>
