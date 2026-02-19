d704f766e7b9d789d9599dc02ea3fa77600d3b99
54Alexzen <alexzen.develop@gmail.com>
2026-02-19 17:04:05 -0600

    feat: mejorar servicio de creación de vehículo con generación de ID y marcas de tiempo

--- src/modules/vehicles/vehicle-services.ts
+++ src/modules/vehicles/vehicle-services.ts
@@ -1,13 +1,19 @@
 import { API_ENDPOINTS } from "@/shared/config/constants";
 import { API_SERVICE } from "@/shared/services/api-service";
 import type { Vehicle, VehicleFilters } from "./vehicle-types";
+import { v4 as uuidv4 } from "uuid";
 
 export const getVehiclesService = (params?: VehicleFilters) => {
   return API_SERVICE.GET<Vehicle[]>(API_ENDPOINTS.VEHICLES, params);
 };
 
 export const createVehicleService = (body: Omit<Vehicle, "id">) => {
-  return API_SERVICE.POST<Vehicle>(API_ENDPOINTS.VEHICLES, body);
+  return API_SERVICE.POST<Vehicle>(API_ENDPOINTS.VEHICLES, {
+    ...body,
+    id: uuidv4(),
+    createdAt: new Date().toISOString(),
+    lastUpdatedAt: new Date().toISOString(),
+  });
 };
 
 export const updateVehicleService = (id: string, body: Omit<Vehicle, "id">) => {
