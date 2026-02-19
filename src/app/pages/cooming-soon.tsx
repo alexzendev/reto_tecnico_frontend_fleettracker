17e2f541c48c4fa06b31ee007da51e420405f34f
54Alexzen <alexzen.develop@gmail.com>
2026-02-19 13:53:09 -0600

    fix: actualizar texto y estilos en el componente CoomingSoon

--- src/app/pages/cooming-soon.tsx
+++ src/app/pages/cooming-soon.tsx
@@ -9,13 +9,13 @@ export default function CoomingSoon({ title }: CoomingSoonProps) {
   return (
     <main className="min-h-[calc(100vh-5rem)] flex flex-col bg-stone-50 dark:bg-stone-900 text-stone-800 dark:text-stone-200 sm:px-4 px-2 py-4">
       <section className="flex flex-col items-center justify-center flex-1">
-        <span className="lg:text-lg sm:text-base text-sm font-semibold text-primary text-center">
-          En construcción
+        <span className="lg:text-base sm:text-sm text-xs font-medium text-primary text-center tracking-tighter">
+          Próximamente
         </span>
-        <h1 className="lg:text-2xl sm:text-xl text-lg text-center font-bold">
-          Próximamente {title}
+        <h1 className="lg:text-3xl sm:text-2xl text-xl text-center font-bold tracking-tight">
+          {title}
         </h1>
-        <p className="lg:text-sm sm:text-xs text-1.5xs text-center mb-5 max-w-72">
+        <p className="lg:text-sm sm:text-xs text-1.5xs text-center mb-5">
           Estamos trabajando para que puedas acceder pronto.
         </p>
         <Link
