17f042d50597dc53b272a4dd9b0096d1bf24393e
54Alexzen <alexzen.develop@gmail.com>
2026-02-20 18:12:30 -0600

    feat: agregar fondo y z-index al encabezado para mejorar la visibilidad

--- src/shared/components/elements/header.tsx
+++ src/shared/components/elements/header.tsx
@@ -10,7 +10,7 @@ export const Header = ({ title }: HeaderProps) => {
     day: "numeric",
   });
   return (
-    <header className="sticky top-0 border-b border-stone-200 dark:border-stone-700 w-full sm:px-4 px-2 flex flex-col justify-center h-20">
+    <header className="sticky top-0 z-10 bg-stone-50 dark:bg-stone-900 border-b border-stone-200 dark:border-stone-700 w-full sm:px-4 px-2 flex flex-col justify-center h-20">
       <h1 className="font-semibold lg:text-2xl sm:text-xl text-lg tracking-tighter mt-1">
         {title}
       </h1>
