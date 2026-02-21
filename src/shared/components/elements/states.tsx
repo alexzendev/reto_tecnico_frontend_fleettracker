36c21b962f630ed73a270ca8f6ec76162310d557
54Alexzen <alexzen.develop@gmail.com>
2026-02-20 23:06:46 -0600

    feat: mejorar la gestión de estados de error y vacío con descripciones opcionales

--- src/shared/components/elements/states.tsx
+++ src/shared/components/elements/states.tsx
@@ -1,7 +1,23 @@
+import { Link } from "react-router-dom";
 import { BoxIcon } from "../icons/box-icon";
 import { ErrorIcon } from "../icons/error-icon";
 
-export const ErrorState = ({ message }: { message: string }) => (
+interface StateProps {
+  message: string;
+  description?: string;
+}
+
+interface ErrorStateProps extends StateProps {
+  to?: string;
+  labelto?: string;
+}
+
+export const ErrorState = ({
+  message,
+  description,
+  to,
+  labelto,
+}: ErrorStateProps) => (
   <div className="text-center py-10 flex flex-col items-center justify-center flex-1">
     <ErrorIcon className="lg:size-16 sm:size-14 size-12 mb-4 text-primary" />
     <span className="lg:text-base sm:text-sm text-xs font-medium text-primary text-center tracking-tighter">
@@ -11,13 +27,20 @@ export const ErrorState = ({ message }: { message: string }) => (
       {message}
     </p>
     <p className="lg:text-xs sm:text-1.5xs text-2xs max-w-md text-center">
-      Intentalo de nuevo más tarde o recarga la página. Si el problema persiste,
-      contacta al soporte técnico.
+      {description || "Intentalo de nuevo más tarde o recarga la página."}
     </p>
+    {to && (
+      <Link
+        to={to}
+        className="bg-primary hover:bg-secondary text-stone-100 px-4 py-2.5 rounded-full sm:text-1.5xs text-2xs uppercase font-semibold flex items-center justify-center gap-1 cursor-pointer transition-all duration-200 group mt-5"
+      >
+        {labelto || "Volver al inicio"}
+      </Link>
+    )}
   </div>
 );
 
-export const EmptyState = ({ message }: { message: string }) => (
+export const EmptyState = ({ message, description }: StateProps) => (
   <div className="text-center py-10 flex flex-col items-center justify-center flex-1">
     <BoxIcon className="lg:size-24 sm:size-20 size-16 mb-4 text-primary" />
     <span className="lg:text-base sm:text-sm text-xs font-medium text-primary text-center tracking-tighter">
@@ -27,7 +50,7 @@ export const EmptyState = ({ message }: { message: string }) => (
       {message}
     </p>
     <p className="lg:text-xs sm:text-1.5xs text-2xs max-w-md text-center">
-      Intentalo de nuevo más tarde o recarga la página.
+      {description || "Intentalo de nuevo más tarde o recarga la página."}
     </p>
   </div>
 );
