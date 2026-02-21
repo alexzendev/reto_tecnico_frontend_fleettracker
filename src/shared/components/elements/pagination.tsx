f7d06673421053554671da1a590b38d564277625
54Alexzen <alexzen.develop@gmail.com>
2026-02-20 18:12:23 -0600

    feat: mejorar estilos de paginación y ajustar clases para mayor consistencia

--- src/shared/components/elements/pagination.tsx
+++ src/shared/components/elements/pagination.tsx
@@ -15,7 +15,7 @@ interface PaginationProps {
 export const Pagination = ({ page, totalPages, setPage }: PaginationProps) => {
   return (
     <div className="mt-5">
-      <p className="text-xs mb-2">
+      <p className="sm:text-xs text-1.5xs font-medium mb-2">
         Página {page} de {totalPages}
       </p>
 
@@ -23,17 +23,17 @@ export const Pagination = ({ page, totalPages, setPage }: PaginationProps) => {
         <button
           onClick={() => setPage(1)}
           disabled={page === 1}
-          className="flex items-center gap-0.5 px-2 py-2 rounded-md bg-stone-100 dark:bg-stone-800 border border-stone-200 dark:border-stone-700 disabled:opacity-40 disabled:cursor-default cursor-pointer"
+          className="flex items-center gap-0.5 sm:p-2 p-1 rounded-md bg-stone-100 dark:bg-stone-800 border border-stone-200 dark:border-stone-700 disabled:opacity-40 disabled:cursor-default cursor-pointer"
         >
           <ChevronsLeft className="size-3.5" />
         </button>
         <button
           onClick={() => setPage((p) => p - 1)}
           disabled={page === 1}
-          className="flex items-center gap-0.5 px-2 py-2 rounded-md bg-stone-100 dark:bg-stone-800 border border-stone-200 dark:border-stone-700 disabled:opacity-40 disabled:cursor-default cursor-pointer"
+          className="flex items-center gap-0.5 sm:p-2 p-1 rounded-md bg-stone-100 dark:bg-stone-800 border border-stone-200 dark:border-stone-700 disabled:opacity-40 disabled:cursor-default cursor-pointer"
         >
-          <ChevronLeft className="size-3.5" />
-          <span className="sm:text-1.5xs text-2xs uppercase font-semibold">
+          <ChevronLeft className="sm:size-3.5 size-3" />
+          <span className="text-2xs uppercase font-semibold sm:flex hidden">
             Anterior
           </span>
         </button>
@@ -41,17 +41,17 @@ export const Pagination = ({ page, totalPages, setPage }: PaginationProps) => {
         <button
           onClick={() => setPage((p) => p + 1)}
           disabled={page === totalPages}
-          className="flex items-center gap-0.5 px-2 py-2 rounded-md bg-stone-100 dark:bg-stone-800 border border-stone-200 dark:border-stone-700 disabled:opacity-40 disabled:cursor-default cursor-pointer"
+          className="flex items-center gap-0.5 sm:p-2 p-1 rounded-md bg-stone-100 dark:bg-stone-800 border border-stone-200 dark:border-stone-700 disabled:opacity-40 disabled:cursor-default cursor-pointer"
         >
-          <span className="sm:text-1.5xs text-2xs uppercase font-semibold">
+          <span className="text-2xs uppercase font-semibold sm:flex hidden">
             Siguiente
           </span>
-          <ChevronRight className="size-3.5" />
+          <ChevronRight className="sm:size-3.5 size-3" />
         </button>
         <button
           onClick={() => setPage(totalPages)}
           disabled={page === totalPages}
-          className="flex items-center gap-0.5 px-2 py-2 rounded-md bg-stone-100 dark:bg-stone-800 border border-stone-200 dark:border-stone-700 disabled:opacity-40 disabled:cursor-default cursor-pointer"
+          className="flex items-center gap-0.5 sm:p-2 p-1 rounded-md bg-stone-100 dark:bg-stone-800 border border-stone-200 dark:border-stone-700 disabled:opacity-40 disabled:cursor-default cursor-pointer"
         >
           <ChevronsRight className="size-3.5" />
         </button>
