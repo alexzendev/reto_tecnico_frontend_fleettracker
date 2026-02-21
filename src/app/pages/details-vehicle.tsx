0693277ff69b80aa07f73a3cf5dfd194b7d9527f
54Alexzen <alexzen.develop@gmail.com>
2026-02-21 13:01:40 -0600

    feat: agregar modales para editar y eliminar vehículo en detalles del vehículo

--- src/app/pages/details-vehicle.tsx
+++ src/app/pages/details-vehicle.tsx
@@ -1,15 +1,19 @@
+import { ModalDeleteVehicle } from "@/modules/vehicles/components/modal-delete-vehicle";
+import { ModalEditVehicle } from "@/modules/vehicles/components/modal-edit-vehicle";
 import { useGetVehicleById } from "@/modules/vehicles/vehicle-hooks";
 import { Header } from "@/shared/components/elements/header";
 import { ErrorState } from "@/shared/components/elements/states";
+import { Button } from "@/shared/components/ui/button";
 import { Loader2 } from "lucide-react";
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 
 export default function DetailsVehicle() {
   const { id } = useParams<{ id: string }>();
-  const { data: vehicle, isLoading, isError, error } = useGetVehicleById(id!);
+  const { data: vehicle, isLoading, isError } = useGetVehicleById(id!);
 
-  console.log(isError);
-  console.log(error);
+  const [isModalEditOpen, setIsModalEditOpen] = useState(false);
+  const [isModalDeleteOpen, setIsModalDeleteOpen] = useState(false);
 
   if (isLoading) {
     return (
@@ -24,17 +28,42 @@ export default function DetailsVehicle() {
     return (
       <div className="min-h-svh flex flex-col items-center justify-center">
         <div>
-          <ErrorState message="Hubo un problema al cargar los detalles del vehículo" description="Intentalo de nuevo más tarde o vuelve a la lista de vehículos" to="/vehicles" labelto="Ir a vehículos" />
+          <ErrorState
+            message="Hubo un problema al cargar los detalles del vehículo"
+            description="Intentalo de nuevo más tarde o vuelve a la lista de vehículos"
+            to="/vehicles"
+            labelto="Ir a vehículos"
+          />
         </div>
       </div>
     );
   }
 
   return (
-    <div className="min-h-svh flex flex-col items-center justify-center">
-      <Header
-        title={`Detalles del vehículo ${vehicle?.brand} - ${vehicle?.model}`}
+    <>
+      <div className="min-h-svh flex flex-col items-center">
+        <Header
+          title={`Detalles del vehículo ${vehicle?.brand} - ${vehicle?.model}`}
+        />
+        <div>
+          <Button onClick={() => setIsModalEditOpen(true)}>
+            Editar vehículo
+          </Button>
+          <Button onClick={() => setIsModalDeleteOpen(true)}>
+            Eliminar vehículo
+          </Button>
+        </div>
+      </div>
+      <ModalEditVehicle
+        isOpen={isModalEditOpen}
+        onClose={() => setIsModalEditOpen(false)}
+        vehicle={vehicle}
       />
-    </div>
+      <ModalDeleteVehicle
+        isOpen={isModalDeleteOpen}
+        onClose={() => setIsModalDeleteOpen(false)}
+        vehicle={vehicle}
+      />
+    </>
   );
 }
