c14a30f4d4827e78de9ff2057ee122c48adeb453
54Alexzen <alexzen.develop@gmail.com>
2026-02-22 01:33:04 -0600

    feat: agregar función para formatear fechas en español

---
+++ src/shared/utils/formated-date.ts
@@ -0,0 +1,8 @@
+export const formatedDate = (date: string): string => {
+  return new Date(date).toLocaleDateString("es-ES", {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+};
