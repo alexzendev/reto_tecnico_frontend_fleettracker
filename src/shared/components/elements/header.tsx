96093d706286ffa51bfef9d44b308d49f1dc63ea
54Alexzen <alexzen.develop@gmail.com>
2026-02-22 01:33:13 -0600

    feat: reemplazar la función de formateo de fecha por una implementación más centralizada

--- src/shared/components/elements/header.tsx
+++ src/shared/components/elements/header.tsx
@@ -1,14 +1,12 @@
+import { formatedDate } from "@/shared/utils/formated-date";
+
+
 interface HeaderProps {
   title: string;
 }
 
 export const Header = ({ title }: HeaderProps) => {
-  const date = new Date().toLocaleDateString("es-ES", {
-    weekday: "long",
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  });
+  const date = formatedDate(new Date().toISOString());
   return (
     <header className="sticky top-0 z-10 bg-stone-50 dark:bg-stone-900 border-b border-stone-200 dark:border-stone-700 w-full sm:px-4 px-2 flex flex-col justify-center h-20">
       <h1 className="font-semibold lg:text-2xl sm:text-xl text-lg tracking-tighter">
