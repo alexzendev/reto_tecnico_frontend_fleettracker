b1395466f76a7b3462dacabc4ff1414795005608
54Alexzen <alexzen.develop@gmail.com>
2026-02-22 01:09:43 -0600

    feat: mejorar la estructura del componente NotFound con ajustes de formato y estilos

--- src/app/pages/not-found.tsx
+++ src/app/pages/not-found.tsx
@@ -11,7 +11,9 @@ export default function NotFound() {
         </div>
       </header>
       <section className="flex flex-col items-center justify-center flex-1">
-        <span className="lg:text-base sm:text-sm text-xs font-medium text-primary text-center tracking-tighter">404</span>
+        <span className="lg:text-base sm:text-sm text-xs font-medium text-primary text-center tracking-tighter">
+          404
+        </span>
         <h1 className="lg:text-3xl sm:text-2xl text-xl text-center font-bold tracking-tight">
           Página no encontrada
         </h1>
@@ -20,7 +22,7 @@ export default function NotFound() {
         </p>
         <Link
           to="/"
-          className="bg-primary hover:bg-secondary text-stone-100 px-5 py-2.5 rounded-full sm:text-xs text-1.5xs uppercase font-semibold flex items-center justify-center gap-1 cursor-pointer transition-all duration-200 group"
+          className="sm:px-4 px-3 sm:py-2 py-1.5 rounded-full lg:text-xs sm:text-1.5xs text-2xs uppercase font-semibold flex items-center justify-center gap-1 cursor-pointer transition-all duration-200 disabled:opacity-80 disabled:cursor-not-allowed disabled:hover:bg-primary group bg-primary hover:bg-secondary text-stone-100 group"
         >
           Volver al inicio
           <ArrowRight className="size-3.5 group-hover:-rotate-45 transition-all duration-200" />
