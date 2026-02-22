52fabd32156fa33ac9df510ac95d16f43cfb7ef0
54Alexzen <alexzen.develop@gmail.com>
2026-02-22 15:51:16 -0600

    feat: mejorar el componente DetailsCard con estilos y estructura actualizados

--- src/modules/vehicles/components/details-card.tsx
+++ src/modules/vehicles/components/details-card.tsx
@@ -3,6 +3,7 @@ import type { LucideIcon } from "lucide-react";
 interface DetailField {
   label: string;
   value: string | number;
+  color?: string;
 }
 
 interface DetailsCardProps {
@@ -19,18 +20,21 @@ export const DetailsCard = ({
   return (
     <div className="rounded-md bg-stone-100 dark:bg-stone-800 lg:p-4 p-2">
       <div className="flex sm:gap-2 gap-1 items-center">
-        <Icon className="size-4" />
-        <h2 className="font-semibold lg:text-sm sm:text-xs text-1.5xs tracking-tighter">
+        <Icon className="size-4 text-primary" />
+        <h2 className="font-semibold lg:text-sm sm:text-xs text-1.5xs uppercase tracking-tighter">
           {title}
         </h2>
       </div>
-      <div className="grid grid-cols-2 sm:gap-3.5 gap-3 mt-5">
+      <div className="grid sm:grid-cols-2 grid-cols-1 sm:gap-3.5 gap-3 mt-5">
         {fields.map((field) => (
           <div key={field.label}>
-            <h3 className="lg:text-xs text-1.5xs font-semibold uppercase">
+            <h3 className="lg:text-xs sm:text-1.5xs text-2xs font-semibold">
               {field.label}
             </h3>
-            <p className="lg:text-xs text-1.5xs">{field.value}</p>
+            <div className="flex items-center sm:gap-1 gap-0.5">
+              {field.color && <div className={`size-1.5 rounded-full ${field.color}`} />}
+              <p className="lg:text-xs text-1.5xs">{field.value}</p>
+            </div>
           </div>
         ))}
       </div>
