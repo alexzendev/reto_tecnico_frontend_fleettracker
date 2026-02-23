e6c95940ab896b5333ffc9080f8b9cb66089a3e2
54Alexzen <alexzen.develop@gmail.com>
2026-02-22 20:47:26 -0600

    feat: renombrar función de formateo de fecha y agregar formato de entrada

--- src/shared/utils/formated-date.ts
+++ src/shared/utils/formated-date.ts
@@ -1,8 +1,12 @@
-export const formatedDate = (date: string): string => {
+export const formatDateLong = (date: string): string => {
   return new Date(date).toLocaleDateString("es-ES", {
     weekday: "long",
     year: "numeric",
     month: "long",
     day: "numeric",
   });
+};
+
+export const formatDateInput = (date: string): string => {
+  return date.split("T")[0];
 };
