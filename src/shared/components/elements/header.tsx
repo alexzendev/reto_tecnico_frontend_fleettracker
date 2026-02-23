0cbda7e4f8c109b22e9a245d6fd8a9345ae85b6d
54Alexzen <alexzen.develop@gmail.com>
2026-02-22 20:47:34 -0600

    fix: corregir nombre de función de formateo de fecha en el componente Header

--- src/shared/components/elements/header.tsx
+++ src/shared/components/elements/header.tsx
@@ -1,4 +1,4 @@
-import { formatedDate } from "@/shared/utils/formated-date";
+import { formatDateLong } from "@/shared/utils/formated-date";
 
 
 interface HeaderProps {
@@ -6,7 +6,7 @@ interface HeaderProps {
 }
 
 export const Header = ({ title }: HeaderProps) => {
-  const date = formatedDate(new Date().toISOString());
+  const date = formatDateLong(new Date().toISOString());
   return (
     <header className="sticky top-0 z-10 bg-stone-50 dark:bg-stone-900 border-b border-stone-200 dark:border-stone-700 w-full sm:px-4 px-2 flex flex-col justify-center h-20">
       <h1 className="font-semibold lg:text-2xl sm:text-xl text-lg tracking-tighter">
