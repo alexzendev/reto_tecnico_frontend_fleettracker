29a0106a70c2709e69c2c10932e93112a2368761
54Alexzen <alexzen.develop@gmail.com>
2026-02-22 23:41:04 -0600

    feat: actualizar el formulario de creación de vehículo con nuevos campos y validaciones

--- src/app/pages/new-vehicle.tsx
+++ src/app/pages/new-vehicle.tsx
@@ -1,8 +1,8 @@
 import { useCreateVehicle } from "@/modules/vehicles/vehicle-hooks";
 import {
-  vehicleSchema,
-  type VehicleFormInput,
-  type VehicleFormOutput,
+  vehicleCreateSchema,
+  type VehicleCreateInput,
+  type VehicleCreateOutput,
 } from "@/modules/vehicles/vehicle-schemas";
 import { Header } from "@/shared/components/elements/header";
 import { Button } from "@/shared/components/ui/button";
@@ -11,7 +11,14 @@ import { Input } from "@/shared/components/ui/input";
 import { Select } from "@/shared/components/ui/select";
 import { DATA_UI } from "@/shared/utils/data-ui";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Loader } from "lucide-react";
+import {
+  Fingerprint,
+  Gauge,
+  Info,
+  Loader2,
+  Settings2,
+  Wrench,
+} from "lucide-react";
 import { useForm } from "react-hook-form";
 
 export default function NewVehicle() {
@@ -22,11 +29,13 @@ export default function NewVehicle() {
     handleSubmit,
     reset,
     formState: { errors },
-  } = useForm<VehicleFormInput, unknown, VehicleFormOutput>({
-    resolver: zodResolver(vehicleSchema),
+  } = useForm<VehicleCreateInput, unknown, VehicleCreateOutput>({
+    resolver: zodResolver(vehicleCreateSchema),
   });
 
-  const onSubmit = (data: VehicleFormOutput) => {
+  const today = new Date().toISOString().split("T")[0];
+
+  const onSubmit = (data: VehicleCreateOutput) => {
     createVehicle(data, {
       onSuccess: () => reset(),
     });
@@ -36,84 +45,241 @@ export default function NewVehicle() {
     <section className="min-h-svh flex flex-col">
       <Header title="Agregar nuevo vehículo" />
       <ButtonBack to="/vehicles" />
-      <div className="flex-1 flex items-center justify-center p-4">
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          className="max-w-2xl w-full space-y-3"
-        >
-          <div className="flex md:flex-row flex-col lg:gap-4 gap-2">
-            <Input
-              placeholder="KJX-482-B"
-              label="Placa vehícular"
-              error={errors.plate?.message}
-              {...register("plate")}
-            />
-            <Input
-              placeholder="Toyota"
-              label="Marca"
-              error={errors.brand?.message}
-              {...register("brand")}
-            />
-          </div>
-
-          <div className="flex md:flex-row flex-col lg:gap-4 gap-2">
-            <Input
-              placeholder="Corolla"
-              label="Modelo"
-              error={errors.model?.message}
-              {...register("model")}
-            />
-
-            <Input
-              type="number"
-              min={0}
-              placeholder="2020"
-              label="Año"
-              error={errors.year?.message}
-              {...register("year")}
-            />
-          </div>
-
-          <div className="flex md:flex-row flex-col lg:gap-4 gap-2">
-            <Input
-              placeholder="Rojo"
-              label="Color"
-              error={errors.color?.message}
-              {...register("color")}
-            />
-
-            <Input
-              type="number"
-              min={0}
-              placeholder="15000"
-              label="Kilometraje"
-              error={errors.mileage?.message}
-              {...register("mileage")}
-            />
-          </div>
-
-          <div className="mb-10">
-            <Select
-              placeholder="Selecciona el estado del vehículo"
-              label="Estado"
-              error={errors.status?.message}
-              options={DATA_UI.options_status_select}
-              {...register("status")}
-            />
-          </div>
-
-          <Button type="submit" disabled={isPending}>
-            {isPending ? (
-              <div className="flex items-center gap-1">
-                <span>Creando vehículo</span>
-                <Loader className="animate-spin size-4" />
+      <div className="flex-1 flex items-center justify-center">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="grid grid-cols-2 gap-4">
+            <div className="rounded-md bg-stone-100 dark:bg-stone-800 lg:p-4 p-2">
+              <div className="flex sm:gap-2 gap-1 items-center mb-3">
+                <Info className="shrink-0 size-4 text-primary" />
+                <h3 className="font-semibold lg:text-sm sm:text-xs text-1.5xs uppercase tracking-tighter">
+                  Información general
+                </h3>
               </div>
-            ) : (
-              <div className="flex items-center gap-1">
-                <span>Agregar vehículo</span>
+              <div className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 lg:gap-4 gap-2">
+                <Input
+                  placeholder="KJX-482-B"
+                  label="Placa"
+                  error={errors.plate?.message}
+                  {...register("plate")}
+                />
+                <Select
+                  placeholder="Selecciona el estado del vehículo"
+                  label="Estado"
+                  error={errors.status?.message}
+                  options={DATA_UI.options_status_select}
+                  {...register("status")}
+                />
+                <Select
+                  placeholder="Tipo de vehículo"
+                  label="Tipo de vehículo"
+                  error={errors.type?.message}
+                  options={DATA_UI.options_type_select}
+                  {...register("type")}
+                />
+                <Input
+                  placeholder="Logística"
+                  label="Departamento"
+                  error={errors.department?.message}
+                  {...register("department")}
+                />
+                <Input
+                  placeholder="Juan Pérez"
+                  label="Conductor asignado"
+                  error={errors.driver?.message}
+                  {...register("driver")}
+                />
               </div>
-            )}
-          </Button>
+            </div>
+
+            <div className="rounded-md bg-stone-100 dark:bg-stone-800 lg:p-4 p-2">
+              <div className="flex sm:gap-2 gap-1 items-center mb-3">
+                <Settings2 className="shrink-0 size-4 text-primary" />
+                <h3 className="font-semibold lg:text-sm sm:text-xs text-1.5xs uppercase tracking-tighter">
+                  Especificaciones del vehículo
+                </h3>
+              </div>
+              <div className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 lg:gap-4 gap-2">
+                <Input
+                  placeholder="Mazda"
+                  label="Marca"
+                  error={errors.brand?.message}
+                  {...register("brand")}
+                />
+                <Input
+                  placeholder="CX-5"
+                  label="Modelo"
+                  error={errors.model?.message}
+                  {...register("model")}
+                />
+                <Input
+                  type="number"
+                  placeholder="1900"
+                  label="Año"
+                  error={errors.year?.message}
+                  {...register("year")}
+                />
+                <Input
+                  placeholder="Rojo"
+                  label="Color"
+                  error={errors.color?.message}
+                  {...register("color")}
+                />
+                <Input
+                  type="number"
+                  placeholder="5"
+                  label="Asientos"
+                  error={errors.seats?.message}
+                  {...register("seats")}
+                />
+                <Input
+                  type="number"
+                  placeholder="4"
+                  label="Puertas"
+                  error={errors.doors?.message}
+                  {...register("doors")}
+                />
+                <Select
+                  placeholder="Tipo de combustible"
+                  label="Tipo de combustible"
+                  error={errors.fuel_type?.message}
+                  options={DATA_UI.options_fuel_type_select}
+                  {...register("fuel_type")}
+                />
+                <Select
+                  placeholder="Tipo de transmisión"
+                  label="Tipo de transmisión"
+                  error={errors.transmission?.message}
+                  options={DATA_UI.options_transmission_select}
+                  {...register("transmission")}
+                />
+              </div>
+            </div>
+
+            <div className="rounded-md bg-stone-100 dark:bg-stone-800 lg:p-4 p-2">
+              <div className="flex sm:gap-2 gap-1 items-center mb-3">
+                <Fingerprint className="shrink-0 size-4 text-primary" />
+                <h3 className="font-semibold lg:text-sm sm:text-xs text-1.5xs uppercase tracking-tighter">
+                  Identificación
+                </h3>
+              </div>
+              <div className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 lg:gap-4 gap-2">
+                <Input
+                  placeholder="1HGCM82633A004352"
+                  label="VIN"
+                  error={errors.vin?.message}
+                  {...register("vin")}
+                />
+                <Input
+                  placeholder="CX-5"
+                  label="Número de motor"
+                  error={errors.engine_number?.message}
+                  {...register("engine_number")}
+                />
+                <Input
+                  placeholder="Guadalajara, Jalisco"
+                  label="Entidad emisora de la licencia"
+                  error={errors.license_state?.message}
+                  {...register("license_state")}
+                />
+              </div>
+            </div>
+
+            <div className="rounded-md bg-stone-100 dark:bg-stone-800 lg:p-4 p-2">
+              <div className="flex sm:gap-2 gap-1 items-center mb-3">
+                <Gauge className="shrink-0 size-4 text-primary" />
+                <h3 className="font-semibold lg:text-sm sm:text-xs text-1.5xs uppercase tracking-tighter">
+                  Operación
+                </h3>
+              </div>
+              <div className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 lg:gap-4 gap-2 mb-3">
+                <Input
+                  type="number"
+                  placeholder="8500"
+                  label="Kilometraje"
+                  error={errors.mileage?.message}
+                  {...register("mileage")}
+                />
+                <Select
+                  placeholder="Selecciona el estado del GPS"
+                  label="Estado del GPS"
+                  error={errors.gps_status?.message}
+                  options={DATA_UI.options_gps_status_select}
+                  {...register("gps_status")}
+                />
+                <Input
+                  placeholder="Guadalajara, Jalisco"
+                  label="Ubicación"
+                  error={errors.location?.message}
+                  {...register("location")}
+                />
+              </div>
+            </div>
+
+            <div className="rounded-md bg-stone-100 dark:bg-stone-800 lg:p-4 p-2">
+              <div className="flex sm:gap-2 gap-1 items-center mb-3">
+                <Wrench className="shrink-0 size-4 text-primary" />
+                <h3 className="font-semibold lg:text-sm sm:text-xs text-1.5xs uppercase tracking-tighter">
+                  Mantenimiento y documentación
+                </h3>
+              </div>
+              <div className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 lg:gap-4 gap-2 mb-3">
+                <Input
+                  type="date"
+                  max={today}
+                  placeholder="2025-03-15"
+                  label="Último mantenimiento"
+                  error={errors.last_service?.message}
+                  {...register("last_service")}
+                />
+                <Input
+                  type="date"
+                  min={today}
+                  placeholder="2025-03-15"
+                  label="Próximo mantenimiento"
+                  error={errors.next_service?.message}
+                  {...register("next_service")}
+                />
+                <Input
+                  type="number"
+                  placeholder="15000"
+                  label="Próximo mantenimiento (Km)"
+                  error={errors.next_service_mileage?.message}
+                  {...register("next_service_mileage")}
+                />
+                <Input
+                  type="date"
+                  min={today}
+                  placeholder="2025-03-15"
+                  label="Vencimiento de seguro"
+                  error={errors.insurance_expiry?.message}
+                  {...register("insurance_expiry")}
+                />
+                <Input
+                  type="date"
+                  min={today}
+                  placeholder="2025-03-15"
+                  label="Vencimiento de tarjeta de circulación"
+                  error={errors.circulation_card_expiry?.message}
+                  {...register("circulation_card_expiry")}
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="mt-6 flex justify-center">
+            <Button type="submit" disabled={isPending}>
+              {isPending ? (
+                <div className="flex items-center gap-1">
+                  <span>Creando vehículo</span>
+                  <Loader2 className="animate-spin size-4" />
+                </div>
+              ) : (
+                <div className="flex items-center gap-1">
+                  <span>Agregar vehículo</span>
+                </div>
+              )}
+            </Button>
+          </div>
         </form>
       </div>
     </section>
